e3fb39062d6d6c7c2e8f2af750c9f59e9b79b7cc
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-11-03 12:08:34 +0100

    Commit

--- proyectos/cucumber/features/steps/animalitos.service.steps.js
+++ proyectos/cucumber/features/steps/animalitos.service.steps.js
@@ -1,65 +1,42 @@
-  Given('que tengo un servicio de animalitos que trabaja con un backend de prueba', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+import { Given, When, Then } from '@cucumber/cucumber';
+import { DatosDeNuevoAnimalito } from '../../DatosDeNuevoAnimalito.js';
+import { AnimalitosService } from '../../ServicioAnimalitos.js';
+import chai from 'chai';
 
-  Given('que tengo un nuevo animalito', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+Given('que tengo un servicio de animalitos que trabaja con un backend de prueba', function () {
+    const URL_BACKEND = "http://localhost:3001/animalitos"
+    this.animalitosService = new AnimalitosService(URL_BACKEND)
+});
 
+Given('que tengo un nuevo animalito', function () {
+    this.nuevoAnimalito= new DatosDeNuevoAnimalito()
+});
 
-  Given('que ese animalito tiene por {string}: {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+Given('que ese animalito tiene por {string}: {string}', function (campo, valor) {
+    this.nuevoAnimalito[campo]=valor
+});
 
+Given('que ese animalito tiene por {string}: {int}', function (campo, valor) {
+    this.nuevoAnimalito[campo]=valor
+});
 
-  Given('que ese animalito tiene por {string}: {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+When('solicito un alta del animalito', async function () {
+    this.animalito = await this.animalitosService.altaDeAnimalito(this.nuevoAnimalito)
+});
 
+Then('se devuelve un animalito', function () {
+    chai.expect(this.animalito).to.be.not.undefined
+});
 
-  Given('que ese animalito tiene por {string}: {int}', function (string, int) {
-  // Given('que ese animalito tiene por {string}: {float}', function (string, float) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+Then('ese animalito tiene por {string}: {string}', function (campo, valor) {
+    chai.expect(this.animalito[campo]).to.be.equal(valor)
+});
 
+Then('ese animalito tiene por {string}: {int}', function (campo, valor) {
+    chai.expect(this.animalito[campo]).to.be.equal(valor)
+});
 
-  When('solicito un alta del animalito', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-
-  Then('se devuelve un animalito', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-
-  Then('ese animalito tiene por {string}: {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-
-  Then('ese animalito tiene por {string}: {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-
-  Then('ese animalito tiene por {string}: {int}', function (string, int) {
-  // Then('ese animalito tiene por {string}: {float}', function (string, float) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-
-  Then('ese animalito tiene un id válido', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+Then('ese animalito tiene un id válido', function () {
+    chai.expect(this.animalito.id).to.be.a('number')
+    chai.expect(this.animalito.id).to.be.above(0)
+});
